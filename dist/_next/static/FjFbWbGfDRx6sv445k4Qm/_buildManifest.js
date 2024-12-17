81101b0ea2831934c0e308c53c5c4fccc0eeb507
Inbestigator <119569726+Inbestigator@users.noreply.github.com>
2024-12-17 14:57:07 -0800

    Added dist

---
+++ dist/_next/static/FjFbWbGfDRx6sv445k4Qm/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/579-794b1b33625d02f3.js","static/chunks/pages/index-a5a67d18460d5318.js"],"/_error":["static/chunks/pages/_error-5feb345ab3932fe4.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
